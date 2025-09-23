fc409755ba5c62365c5d8cd5671e9cf7d0b31402
am-goku <amkrishna2483@gmail.com>
2025-09-23 19:32:03 +0530

    bug-fix: updated logout bug in the user initializer

--- src/components/initializers/UserInitializer.tsx
+++ src/components/initializers/UserInitializer.tsx
@@ -2,19 +2,21 @@
 
 import apiInstance from "@/lib/axios";
 import { useUserStore } from "@/stores/userStore";
+import { useSession } from "next-auth/react";
 import { useEffect } from "react"
 
 function UserInitializer() {
 
+    const { data: session } = useSession();
+
     const fetchUser = async () => {
         const user = (await apiInstance.get("/api/auth/user")).data;
         useUserStore.getState().setUser(user);
-        console.log("user from initializer", user);
     }
 
     useEffect(() => {
-        fetchUser();
-    }, [])
+        if (session) fetchUser();
+    }, [session])
 
     return null
 }
